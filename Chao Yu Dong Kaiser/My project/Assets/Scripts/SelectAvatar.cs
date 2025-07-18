16da355e05bc3394ea3d6fa695bb4271495e8a92
Zhou Hong <zhouhong@tp.edu.sg>
2025-07-18 11:33:56 +0800

    update ZH Scene

--- Chao Yu Dong Kaiser/My project/Assets/Scripts/SelectAvatar.cs
+++ Chao Yu Dong Kaiser/My project/Assets/Scripts/SelectAvatar.cs
@@ -1,17 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEditor.Animations;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class SelectAvatar : MonoBehaviour
 {
     public List<GameObject> avatars = new List<GameObject>();
+    public TMP_Text promptText;
     // Start is called before the first frame update
     void Start()
     {
-        Animator[] animators = FindObjectsOfType<Animator>();
-        foreach (Animator animator in animators) avatars.Add(animator.gameObject);
+        TMP_Text[] texts = FindObjectsOfType<TMP_Text>(true);
+        foreach (TMP_Text text in texts)
+        {
+            if (text.gameObject.name == "AvatarSelected")
+            {
+                promptText = text;
+                return;
+            }
+        }
     }
 
     // Update is called once per frame
@@ -27,5 +39,8 @@ public class SelectAvatar : MonoBehaviour
         Animator animator = transform.GetComponent<Animator>();
         GoScene goScene = FindObjectOfType<GoScene>();
         goScene.animator = animator;
+                promptText.text = transform.gameObject.name + " is selected.";
+        Outline[] outlines = FindObjectsOfType<Outline>();
+        foreach (Outline outline in outlines) outline.enabled = false;
     }
 }
