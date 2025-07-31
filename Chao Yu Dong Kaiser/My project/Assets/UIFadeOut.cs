c68033328b3f2ced40215b6bb3030e209567c3cb
Kaiser Chao <2300345B@student.tp.edu.sg>
2025-08-01 02:48:38 +0800

    Added in more fadein and fadeout for ui into panels

--- Chao Yu Dong Kaiser/My project/Assets/UIFadeOut.cs
+++ Chao Yu Dong Kaiser/My project/Assets/UIFadeOut.cs
@@ -2,30 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIFadeIn : MonoBehaviour
+public class UIFadeOut : MonoBehaviour
+
 {
     public CanvasGroup canvasGroup;
     public float fadeDuration = 1f;
 
     public void FadeNow()
     {
-        gameObject.SetActive(true); // make sure it's visible
         StopAllCoroutines();
-        StartCoroutine(FadeInRoutine());
+        StartCoroutine(FadeOutRoutine());
     }
 
-    private IEnumerator FadeInRoutine()
+    private IEnumerator FadeOutRoutine()
     {
         float time = 0f;
-        canvasGroup.alpha = 0f;
+        canvasGroup.alpha = 1f;
 
         while (time < fadeDuration)
         {
-            canvasGroup.alpha = Mathf.Lerp(0f, 1f, time / fadeDuration);
+            canvasGroup.alpha = Mathf.Lerp(1f, 0f, time / fadeDuration);
             time += Time.deltaTime;
             yield return null;
         }
 
-        canvasGroup.alpha = 1f;
+        canvasGroup.alpha = 0f;
+        gameObject.SetActive(false);
     }
 }
